097d46fc5152c009cd406701cc4908e0c697597a
sangholim <55565835+sangholim@users.noreply.github.com>
2023-03-30 15:12:36 +0900

    feat: database 연결 설정 추가 (#6)
    * chore: build.gradle.kts 수정
    - spring reactive jdbc 라이브러리 추가
    - postgresql r2dbc driver 추가
    
    * refactor: application-local.yml 수정
    - r2dbc.url 경로 추가
    
    * feat: application.yml 추가
    
    * feat: docker-compose 추가
    
    * refactor: .gitignore 수정
    - 경로 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,20 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	implementation("org.postgresql:r2dbc-postgresql:1.0.1.RELEASE")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.security:spring-security-test")
