b23f310c1df0100d24ef3ce6a49ac2828a61c76f
hoya <hybriddevslim@gmail.com>
2023-03-30 14:27:38 +0900

    feat: 애플리케이션 기본 프로젝트 추가

---
+++ src/main/kotlin/com/talk/memberService/MemberServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.talk.memberService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MemberServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MemberServiceApplication>(*args)
+}
