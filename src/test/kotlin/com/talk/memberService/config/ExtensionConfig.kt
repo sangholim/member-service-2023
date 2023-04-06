7882cf7589f3d58d24db971d124d7c7eb4982500
hoya <hybriddevslim@gmail.com>
2023-04-06 12:11:58 +0900

    test: ExtensionConfig 수정 - r2dbcUrl 설정 수정

--- src/test/kotlin/com/talk/memberService/config/ExtensionConfig.kt
+++ src/test/kotlin/com/talk/memberService/config/ExtensionConfig.kt
@@ -3,18 +3,18 @@ package com.talk.memberService.config
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
 import io.kotest.extensions.spring.SpringExtension
-import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.utility.DockerImageName
 
 object ExtensionConfig: AbstractProjectConfig() {
-    val postgresql = PostgreSQLContainer<Nothing>("postgres:11.1").apply {
+    val postgresql = PostgreSQLContainer<Nothing>("postgres:latest").apply {
         this.withUsername("root")
         this.withPassword("password")
-        this.withDatabaseName("authorization")
+        this.withDatabaseName("member_service")
     }
     init {
         postgresql.start()
+        val r2dbcUrl = "r2dbc:postgresql://${postgresql.username}:${postgresql.password}@${postgresql.host}:${postgresql.getMappedPort(5432)}/member_service"
+        System.setProperty("spring.r2dbc.url", r2dbcUrl)
         System.setProperty("spring.flyway.url", postgresql.jdbcUrl)
         System.setProperty("spring.flyway.username", postgresql.username)
         System.setProperty("spring.flyway.password", postgresql.password)
