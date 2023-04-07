67293a311358f3c34d7f0e08c1c2fe8bbcae564f
hoya <hybriddevslim@gmail.com>
2023-04-07 09:33:06 +0900

    refactor: SecurityConfig 수정 - 토큰 인증시 권한 검사 로직 추가

--- src/main/kotlin/com/talk/memberService/config/SecurityConfig.kt
+++ src/main/kotlin/com/talk/memberService/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.talk.memberService.config
 
+import com.talk.memberService.token.CustomAuthoritiesOpaqueTokenIntrospector
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -7,7 +8,6 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.config.web.server.invoke
-import org.springframework.security.oauth2.server.resource.introspection.NimbusReactiveOpaqueTokenIntrospector
 import org.springframework.security.oauth2.server.resource.introspection.ReactiveOpaqueTokenIntrospector
 
 @Configuration
@@ -19,7 +19,7 @@ class SecurityConfig(
     fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
             http {
                 authorizeExchange {
-                    authorize(anyExchange, authenticated)
+                    authorize(anyExchange, hasRole("USER"))
                 }
                 oauth2ResourceServer {
                     opaqueToken {
@@ -30,8 +30,7 @@ class SecurityConfig(
 
     @Bean
     fun introspector(): ReactiveOpaqueTokenIntrospector {
-        val opaque = oAuth2ResourceServerProperties.opaquetoken
-        return NimbusReactiveOpaqueTokenIntrospector(opaque.introspectionUri, opaque.clientId, opaque.clientSecret)
+        return CustomAuthoritiesOpaqueTokenIntrospector(oAuth2ResourceServerProperties.opaquetoken)
     }
 
 }
