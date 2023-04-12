141b8ab90d507f7cceba86dff549e90455ec186e
hoya <hybriddevslim@gmail.com>
2023-04-12 17:31:49 +0900

    refactor: ProfileController.kt 수정 - 프로필 생성 로직 추가 - GetMapping 로직 제거

--- src/main/kotlin/com/talk/memberService/profile/ProfileController.kt
+++ src/main/kotlin/com/talk/memberService/profile/ProfileController.kt
@@ -1,17 +1,19 @@
 package com.talk.memberService.profile
 
+import com.talk.memberService.oauth2.subject
+import org.springframework.http.MediaType
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(value = ["/member-service"])
-class ProfileController {
-
-    @GetMapping(value = ["/profiles"])
-    suspend fun test(@AuthenticationPrincipal principal: OAuth2AuthenticatedPrincipal) {
+class ProfileController(
+        private val profileService: ProfileService
+) {
+    @PostMapping(value = ["/profiles"], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun create(@AuthenticationPrincipal principal: OAuth2AuthenticatedPrincipal, @RequestBody payload: ProfileCreationPayload) {
+        profileService.create(principal.subject(), payload)
     }
 }
 
