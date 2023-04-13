1a3bf887fb49fd2ac4aeda0b9889e58dcf305443
hoya <hybriddevslim@gmail.com>
2023-04-13 09:06:31 +0900

    refactor: ProfileRepository.kt 수정 - userId 기준 프로필 조회 로직 추가

--- src/main/kotlin/com/talk/memberService/profile/ProfileRepository.kt
+++ src/main/kotlin/com/talk/memberService/profile/ProfileRepository.kt
@@ -2,8 +2,10 @@ package com.talk.memberService.profile
 
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface ProfileRepository: CoroutineCrudRepository<Profile, String> {
+interface ProfileRepository : CoroutineCrudRepository<Profile, String> {
     suspend fun countByUserId(userId: String): Int
 
     suspend fun countByEmail(email: String): Int
+
+    suspend fun findByUserId(userId: String): Profile?
 }
