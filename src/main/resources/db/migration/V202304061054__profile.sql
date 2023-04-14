32619bb83039229233cb707f25d51a6ea4316041
sangholim <55565835+sangholim@users.noreply.github.com>
2023-04-14 11:37:55 +0900

    feat: auditAware 추가 (#26)
    * refactor: R2dbcConfig.kt 수정
    - 인증 기반 auditing 작성 로직 추가
    
    * refactor: schema 수정
    - create_by, update_by 필드 타입 수정
    
    * refactor: application-local.yml 수정
    - springdocs 비활성화
    
    * test: springdocs 비활성화, 테스트 케이스 수정

--- src/main/resources/db/migration/V202304061054__profile.sql
+++ src/main/resources/db/migration/V202304061054__profile.sql
@@ -4,8 +4,8 @@ CREATE TABLE profile (
     email varchar(100) UNIQUE NOT NULL,
     name varchar(100) UNIQUE NOT NULL,
     created_at timestamp with time zone DEFAULT NULL,
-    created_by timestamp with time zone DEFAULT NULL,
+    created_by varchar(100) DEFAULT NULL,
     updated_at timestamp with time zone DEFAULT NULL,
-    updated_by timestamp with time zone DEFAULT NULL,
+    updated_by varchar(100) DEFAULT NULL,
     PRIMARY KEY (id)
 );
