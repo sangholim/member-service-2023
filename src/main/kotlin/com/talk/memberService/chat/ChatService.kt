949ac5aceac7d8ae374cbefc5b96ec8a7a0940dd
hoya <hybriddevslim@gmail.com>
2023-04-27 09:39:28 +0900

    refactor: ChatService.kt 수정 - 묶여진 프로필 순차 id 리스트 기반 조회 로직 추가 - 채팅 생성 로직 수정

--- src/main/kotlin/com/talk/memberService/chat/ChatService.kt
+++ src/main/kotlin/com/talk/memberService/chat/ChatService.kt
@@ -8,9 +8,23 @@ class ChatService(
         private val repository: ChatRepository
 ) {
     /**
+     * 묶여진 프로필 순차 id 리스트 기반 조회
+     */
+    suspend fun getByCombinedParticipantProfileSequenceId(combinedParticipantProfileSequenceId: String): Chat? =
+            repository.findByCombinedParticipantProfileSequenceId(combinedParticipantProfileSequenceId)
+
+    /**
+     * 묶여진 프로필 순차 id 기준 존재 여부 체크
+     */
+    suspend fun existByCombinedParticipantProfileSequenceId(combinedParticipantProfileSequenceId: String): Boolean =
+            getByCombinedParticipantProfileSequenceId(combinedParticipantProfileSequenceId) != null
+
+    /**
      * 채팅 도메인 생성
      */
-    suspend fun create(): Chat = chat {
+    suspend fun create(combinedParticipantProfileSequenceId: String, participantCount: Int): Chat = chat {
         this.image = ChatConstant.DEFAULT_CHAT_IMAGE
+        this.combinedParticipantProfileSequenceId = combinedParticipantProfileSequenceId
+        this.participantCount = participantCount
     }.run { repository.save(this) }
 }
