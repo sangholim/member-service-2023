3125b11f5d4baa226b0d62c77c7633e5116b638a
hoya <hybriddevslim@gmail.com>
2023-04-27 09:39:13 +0900

    refactor: ChatRepository.kt 수정 - 묶여진 프로필 순차 id 리스트 기반 조회 로직 추가

--- src/main/kotlin/com/talk/memberService/chat/ChatRepository.kt
+++ src/main/kotlin/com/talk/memberService/chat/ChatRepository.kt
@@ -3,4 +3,9 @@ package com.talk.memberService.chat
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import java.util.*
 
-interface ChatRepository: CoroutineCrudRepository<Chat, UUID>
+interface ChatRepository : CoroutineCrudRepository<Chat, UUID> {
+    /**
+     * 참가자 프로필 순차 id 리스트 기반 조회
+     */
+    suspend fun findByCombinedParticipantProfileSequenceId(combinedParticipantProfileSequenceId: String): Chat?
+}
