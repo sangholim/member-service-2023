8d155eedbe4584d3a14546c67fb39a6429ca1998
hoya <hybriddevslim@gmail.com>
2023-04-30 13:51:01 +0900

    refactor: ProfileController.kt 수정 - 프로필 기준 채팅방 조회 로직 제거

--- src/main/kotlin/com/talk/memberService/profile/ProfileController.kt
+++ src/main/kotlin/com/talk/memberService/profile/ProfileController.kt
@@ -1,8 +1,6 @@
 package com.talk.memberService.profile
 
 import com.talk.memberService.oauth2.subject
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import org.springframework.http.MediaType
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal
@@ -22,11 +20,6 @@ class ProfileController(
     @PostMapping(value = ["/profiles"], consumes = [MediaType.APPLICATION_JSON_VALUE])
     suspend fun create(@AuthenticationPrincipal principal: OAuth2AuthenticatedPrincipal, @RequestBody payload: ProfileCreationPayload) {
         profileService.create(principal.subject(), payload)
-    }
-
-    @GetMapping(value = ["/profile/chats"])
-    suspend fun getAllWithChatViews(@AuthenticationPrincipal principal: OAuth2AuthenticatedPrincipal): Flow<ProfileChatView> {
-        return profileQueryService.getAllWithChats(principal.subject()).map(ProfileChatDto::toView)
     }
 }
 
