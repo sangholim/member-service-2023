a302f7c78cc4dd8d03be6f0c2558d6984d16b5fd
hoya <hybriddevslim@gmail.com>
2023-05-12 22:33:06 +0900

    refactor: ProfileChatView.kt 수정 - 불필요한 로직 제거

--- src/main/kotlin/com/talk/memberService/profile/ProfileChatView.kt
+++ src/main/kotlin/com/talk/memberService/profile/ProfileChatView.kt
@@ -1,6 +1,5 @@
 package com.talk.memberService.profile
 
-import com.talk.memberService.chat.ChatConstant
 import java.util.*
 
 data class ProfileChatView(
@@ -15,14 +14,6 @@ fun ProfileChatDto.toView() = ProfileChatView(
         id = this.id,
         chatId = this.chatId,
         roomName = this.roomName!!,
-        image = this.image,
-        participantCount = this.participantCount
-)
-
-fun ProfileChatDto.toDetailView() = ProfileChatView(
-        id = this.id,
-        chatId = this.chatId,
-        roomName = this.roomName ?: ChatConstant.DEFAULT_CHAT_ROOM_NAME,
         image = this.image,
         participantCount = this.participantCount
 )
